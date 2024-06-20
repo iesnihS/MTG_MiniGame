ea5df4bd6b5f45e78de9dba7df89878d709ac9d8
Samuel Suzan <fullcandies@gmail.com>
2024-06-20 21:47:38 +0200

    Init Project

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,75 @@
+export type Card = {
+  creature: number;
+  instant: number;
+  sorcery: number;
+  artifact: number;
+  enchantment: number;
+  battle: number;
+  planeswalker: number;
+  land: number;
+  basic: number;
+  nonbasic: number;
+  container: {
+    json_dict: {
+      card: {
+        inclusion: number;
+        num_decks: number;
+        potential_decks: number;
+        aetherhub_uri: string;
+        archidekt_uri: string;
+        color_identity: [string, string];
+        cmc: number;
+        deckstats_uri: string;
+        image_uris: [
+          {
+            normal: string;
+            art_crop: string;
+          }
+        ];
+        layout: string;
+        moxfield_uri: string;
+        mtggoldfish_uri: string;
+        name: string;
+        names: [string];
+        prices: {
+          cardhoarder: {
+            price: number;
+            url: string;
+          };
+          cardkingdom: {
+            price: number;
+            url: string;
+          };
+          cardmarket: {
+            price: number;
+            set: string;
+            url: string;
+          };
+          face2face: {
+            price: number;
+            url: string;
+          };
+          mtgstocks: string;
+          scg: { price: number; slug: string };
+          tcgplayer: {
+            price: number;
+            url: string;
+          };
+          tcgl: { price: number; slug: string };
+        };
+        primary_type: string;
+        rarity: string;
+        salt: number;
+        sanitized: string;
+        sanitized_wo: string;
+        scryfall_uri: string;
+        spellbook_uri: string;
+        type: string;
+        combos: boolean;
+        label: string;
+        new: boolean;
+        url: string;
+      };
+    };
+  };
+};
