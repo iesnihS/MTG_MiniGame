b51b21753d082b78ef76e8a1af738140fc48e318
Samuel Suzan <fullcandies@gmail.com>
2024-07-14 12:09:49 +0200

    Change Deployment URL

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default async function Card (){
 const getRandomCardFromDB = async()=>
     {
-      const data =  await fetch("http://localhost:3001/cardAPI/randomcard",{
+      const data =  await fetch("https://vercel.com/iesnihss-projects/mtg-mini-game-back/cardAPI/randomcard",{
         cache: "no-store",
       });
       return data;
