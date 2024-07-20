88e42d21aaf3454f00b080d002213c82a2f3914f
Samuel Suzan <fullcandies@gmail.com>
2024-07-20 11:33:05 +0200

    Add Next config header params

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
